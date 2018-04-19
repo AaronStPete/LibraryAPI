45dca570b2ff576467b26e167c84c985ac96034c
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-19 17:31:45 -0400

    GET all books api call functional

---
+++ LibraryAPI/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using LibraryAPI.Context;
+using LibraryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/books")]
+    public class BookController : ApiController
+    {
+        public IEnumerable<Book> GetAll()
+        {
+            // get all books from the database
+            var db = new LibraryContext();
+            return db.Books;
+        }
+    }
+}
+
