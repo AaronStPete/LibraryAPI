82582db3b0908db5b4943c3e5bd839cc1bb55a3d
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-20 15:14:48 -0400

    realized I have to change methodologies away from routing into view models

--- LibraryAPI/Controllers/BookController.cs
+++ LibraryAPI/Controllers/BookController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.Entity;
 
 namespace LibraryAPI.Controllers
 {
@@ -18,6 +19,42 @@ namespace LibraryAPI.Controllers
             var db = new LibraryContext();
             return db.Books;
         }
+
+        //search by title
+        //GET api/books/title
+        //http://localhost:56372/api/books?title=The_Big_Test
+        [Route("title/{title}")]
+        public IEnumerable<Book> GetByTitle(string title)
+        {
+            var db = new LibraryContext();
+            title = title.Replace('_', ' ');
+            return db.Books.Where(w => w.Title.Contains(title));
+        }
+
+        //search by author
+        //GET api/books/author
+        //http://localhost:56372/api/books?author=A'abohrn
+        [Route("author/{author}")]
+        public IEnumerable<Book> GetByAuthor(string author)
+        {
+            var db = new LibraryContext();
+            var query = db.Books.Include(i => i.Author);
+            author = author.Replace('_', ' ');
+            return query.Where(w => w.Author.Name == author);
+        }
+
+        //search by genre
+        //GET api/books/genre
+
+        [Route("genre{genre}")]
+        public IEnumerable<Book> GetByGenre(string genre)
+        {
+            var db = new LibraryContext();
+            var query = db.Books.Include(i => i.Genre);
+            genre = genre.Replace('_', ' ');
+            return query.Where(w => w.Genre.Name == genre);
+        }
+
     }
 }
 
