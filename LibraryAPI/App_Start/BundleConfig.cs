37722c1c0ab85859f1d000eb42f71bbe71a7adba
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-23 13:34:41 -0400

    Added angular and some basic elements

--- LibraryAPI/App_Start/BundleConfig.cs
+++ LibraryAPI/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace LibraryAPI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
